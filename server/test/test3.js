fb7eb13d809c148979366c80f2256def37b8125d
SciBridge <scibridgelabs@gmail.com>
2024-05-01 12:22:19 +0530

    final merge from sub-branch

--- server/test/test3.js
+++ server/test/test3.js
@@ -7,11 +7,11 @@ function productFunc(a, b) {
   return a * b
 }
 
-console.log(minusFunction(1, 2)) 
-console.log(productFunc(1, 2))
 
 function divideFunc(a, b) {
   return a / b
 }
 
 console.log(divideFunc(1, 2))
+console.log(minusFunction(1, 2)) 
+console.log(productFunc(1, 2))
