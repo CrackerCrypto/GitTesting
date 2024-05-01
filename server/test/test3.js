2bab1a62f2965679509db0118bbc83d8892fc8db
SciBridge <scibridgelabs@gmail.com>
2024-05-01 12:18:20 +0530

    again some changes on main

--- server/test/test3.js
+++ server/test/test3.js
@@ -2,11 +2,11 @@ function minusFunction(a, b) {
   return a - b
 }
 
-console.log(minusFunction(1, 2)) 
 
 function productFunc(a, b) {
   return a * b
 }
 
+console.log(minusFunction(1, 2)) 
 console.log(productFunc(1, 2))
 
